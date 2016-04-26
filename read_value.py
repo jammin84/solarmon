6c61d266348dc1a4032a92d1b8009b029fa8e03d
jammin84 <jammin84@users.noreply.github.com>
2016-04-26 16:13:10 +0800

    use port configured in config file.
    remove port hardcoding

--- read_value.py
+++ read_value.py
@@ -6,11 +6,24 @@
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 import time
 import sys
+import ConfigParser
+import os
+  
+config = os.path.dirname(os.path.realpath(__file__)) + "/solarmon.cfg"
+ 
+if (false == os.path.isfile(config) ) {
+ print "error: 999, errormsg: %s file not found" % (config)
+ return 999
+}
+
+settings = ConfigParser.RawConfigParser()
+settings.read(config)
+inverterPort = settings.get('inverter', 'port')
 
 start = int(sys.argv[1])
 count = int(sys.argv[2])
 
-client = ModbusClient(method='rtu', port='/dev/ttyUSB0', baudrate=9600, stopbits=1, parity='N', bytesize=8, timeout=1)
+client = ModbusClient(method='rtu', port=inverterPort, baudrate=9600, stopbits=1, parity='N', bytesize=8, timeout=1)
 client.connect()
 
 rr = client.read_input_registers(start,count)
